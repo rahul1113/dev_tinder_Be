ca636dcb824741820e7904db5d2de82cf7396555
rahul burnwal <rahulb631@gmail.com>
2025-06-30 07:10:36 +0530

    Routing pattern

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
 
+//Order of routing is important
+// | Pattern | Meaning                          | Example  | Matches              |
+// | ------- | -------------------------------- | -------- | -------------------- |
+// | `?`     | 0 or 1 occurrence                | `/ab?cd` | `/acd`, `/abcd`      |
+// | `+`     | 1 or more occurrences            | `/ab+cd` | `/abcd`, `/abbbbbcd` |
+// | `*`     | Wildcard (any chars, any length) | `/ab*cd` | `/abcd`, `/ab123cd`  |
+
+app.use("/hello", (req, res) => {
+  res.send("Hello ");
+});
 app.use("/", (req, res) => {
   res.send("Hello World");
 });
-
 app.listen(7777, () => {
   console.log("Server is running on port 7777");
 });
